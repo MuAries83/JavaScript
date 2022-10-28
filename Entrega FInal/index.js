0459bd301130201e63c847ec564eb985a778926e
Ernesto Cabrera <ayu_san@hotmail.com>
2022-10-28 14:58:54 -0500

    se termina la municion y la recarga

--- Entrega FInal/index.js
+++ Entrega FInal/index.js
@@ -13,19 +13,35 @@ window.addEventListener("load", function(){
                         (e.key === "ArrowDown")
                 ) && this.juego.keys.indexOf(e.key) === -1){
                     this.juego.keys.push(e.key);
+                } else if (e.key === " "){
+                    this.juego.jugador.shootTop();
                 }
-                console.log(this.juego.keys);
             });
             window.addEventListener("keyup", e => {
                 if(this.juego.keys.indexOf(e.key) > -1){
                     this.juego.keys.splice(this.juego.keys.indexOf(e.key), 1);
                 }
-                console.log(this.juego.keys);
             });
         }
     }
     class Proyectil {
-
+        constructor(juego, x, y){
+            this.juego = juego;
+            this.x = x;
+            this.y = y;
+            this.width = 10;
+            this.height = 3;
+            this.vel = 3;
+            this.marcaEliminacio = false;
+        }
+        update(){
+            this.x += this.vel;
+            if (this.x > this.juego.width * 0.8) this.marcaEliminacio = true;
+        }
+        draw(context){
+            context.fillStyle = "yellow";
+            context.fillRect(this.x, this.y, this.width, this.height);
+        }
     }
 
     class Particula {
@@ -41,15 +57,31 @@ window.addEventListener("load", function(){
             this.y = 100;
             this.movimientoY = 0;
             this.velMaxima = 2;
+            this.proyectiles = [];
         }
         update(){
             if(this.juego.keys.includes("ArrowUp")) this.movimientoY = -this.velMaxima;
                 else if (this.juego.keys.includes("ArrowDown")) this.movimientoY = this.velMaxima;
                     else this.movimientoY = 0;
             this.y += this.movimientoY;
+            // Manejo de proyectiles
+            this.proyectiles.forEach(proyectiles =>{
+                proyectiles.update();
+            });
+            this.proyectiles = this.proyectiles.filter(proyectiles => !proyectiles.marcaEliminacio);
         }
         draw(context){
+            context.fillStyle = "black";
             context.fillRect(this.x, this.y, this.width, this.height);
+            this.proyectiles.forEach(proyectiles =>{
+                proyectiles.draw(context);
+            });
+        }
+        shootTop(){
+            if (this.juego.municion > 0){
+                this.proyectiles.push(new Proyectil(this.juego, this.x + 80, this.y + 30));
+                this.juego.municion --;
+            }
         }
     }
     class Enemigo {
@@ -68,21 +100,34 @@ window.addEventListener("load", function(){
             this.jugador = new Jugador(this);
             this.entrada = new ManejadorEntradas(this);
             this.keys = [];
+            this.municion = 20;
+            this.municionMax = 50;
+            this.recarga = 0;
+            this.iterReca = 500;
         }
-        update(){
+        update(timepodelta){
             this.jugador.update();
+            if (this.recarga > this.iterReca){
+                if(this.municion < this.municionMax) this.municion ++;
+                this.recarga = 0;
+            } else {
+                this.recarga += timepodelta;
+            }
         }
         draw(context){
             this.jugador.draw(context);
         }
     }
     const juego = new Juego(marco.width, marco.height);
+    let momento = 0;
     //construccion de animacion
-    function animar(){
+    function animar(marcaTiempo){
+        const timepodelta = marcaTiempo - momento;
+        momento = marcaTiempo;
         ctx.clearRect(0, 0, marco.width, marco.height);
-        juego.update();
+        juego.update(timepodelta);
         juego.draw(ctx);
         requestAnimationFrame(animar);
     }
-    animar();
+    animar(0);
 })
